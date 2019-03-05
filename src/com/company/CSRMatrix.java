1523dc7f357f2b281d390110290989ec7f0403ac
ptmdmusique <37349324+ptmdmusique@users.noreply.github.com>
2019-03-05 00:18:23 -0800

    3/5/2019 12:16AM
    Finish converting from Double to BigDecimal. Fix bugs with reading from files. Add readings vectors from files.
    
    Next: Add writing to files. Use Plotly to draw graphs.

--- src/com/company/CSRMatrix.java
+++ src/com/company/CSRMatrix.java
@@ -110,6 +110,10 @@ class CSRMatrix {
         for(String row : rows){
             //Find the number of non zero
             for(String number: row.split(WHITESPACE)){
+                if (number.equals("") || number.equals(WHITESPACE)){
+                    continue;
+                }
+                number = number.replaceAll("\\s+","");
                 if (!number.equals("0")){
                     nnz++;
                 }
@@ -128,6 +132,9 @@ class CSRMatrix {
                 int curCol = 0;
                 row[curRow] = totalCol;
                 for(String number : rows[curRow++].split(WHITESPACE)){
+                    if (number.equals("") || number.equals(WHITESPACE)){
+                        continue;
+                    }
                     number = number.replaceAll("\\s+","");
                     if (!number.equals("0")){
                         data[totalCol++] = new Data(BigDecimal.valueOf(Double.parseDouble(number)), curCol);
@@ -163,7 +170,7 @@ class CSRMatrix {
             if (rowStartIndx > rowEndIndx){
                 //Empty row, all 0
                 for(int indx = 0; indx < colSize; indx++){
-                    System.out.printf("%6.2f ", 0.0);
+                    System.out.printf("%" + Main.MAX_SLOT + "." + Main.PRECISION + "f ", 0.0);
                 }
             } else {
                 int curIndx = rowStartIndx;
@@ -171,13 +178,13 @@ class CSRMatrix {
                     if (curIndx < data.length && data[curIndx].col == curCol){
                         //If the current column matches the column of the non-zero entries we are looking at
                         //  then move on with the index and print out the corresponding data
-                        System.out.printf("%6.2f ", data[curIndx].data);
+                        System.out.printf("%" + Main.MAX_SLOT + "." + Main.PRECISION + "f ", data[curIndx].data);
                         if(curIndx < rowEndIndx){
                             curIndx++;
                         }
                     } else {
                         //If not then just print 0
-                        System.out.printf("%6.2f ", 0.0);
+                        System.out.printf("%" + Main.MAX_SLOT + "." + Main.PRECISION + "f ", 0.0);
                     }
                 }
             }
@@ -190,10 +197,13 @@ class CSRMatrix {
             return;
         }
 
-        System.out.printf("%6s %6s %6s\n", "Data", "Col", "Row");
+        System.out.printf("%" + Main.MAX_SLOT + "s " + "%" + Main.MAX_SLOT + "s " + "%" + Main.MAX_SLOT + "s\n", "Data", "Col", "Row");
+        //System.out.printf("%6s %6s %6s\n", "Data", "Col", "Row");
         for(int curRow = 0; curRow < row.length; curRow++){
             for(int indx = row[curRow]; indx <= (curRow == row.length - 1 ? data.length - 1 : row[curRow + 1] - 1); indx++){
-                System.out.printf("%6.2f %6d %6d\n", data[indx].data, data[indx].col, curRow);
+                System.out.printf("%" + Main.MAX_SLOT + "." + Main.PRECISION + "f %" + Main.MAX_SLOT + "d %" + Main.MAX_SLOT + "d\n"
+                        , data[indx].data, data[indx].col, curRow);
+                //System.out.printf("%6.2f %6d %6d\n", data[indx].data, data[indx].col, curRow);
             }
         }
     }
@@ -265,7 +275,7 @@ class CSRMatrix {
         for(int curRow = 0; curRow < row.length; curRow++){
             BigDecimal temp = BigDecimal.ZERO;
             for(int indx = row[curRow]; indx <= (curRow == row.length - 1 ? data.length - 1 : (row[curRow + 1] - 1)); indx++){
-                temp = temp.add(data[indx].data.multiply(parm.GetEntry(data[indx].col)));
+                temp = temp.add(data[indx].data.multiply(parm.GetEntry(data[indx].col), Main.mathContext), Main.mathContext);
             }
             result.SetEntry(curRow, temp);
         }
@@ -313,7 +323,7 @@ class CSRMatrix {
                         //  if the col of the rowCurCol doesn't match the outer loop,
                         //  it means the parm[parmData[rowCurCol[data[k].col]].col][parmCol] == 0
                         //Thus, we don't need to include it in our calculation
-                        temp = temp.add(data[k].data.multiply(parmData[rowCurCol[data[k].col]].data));
+                        temp = temp.add(data[k].data.multiply(parmData[rowCurCol[data[k].col]].data, Main.mathContext), Main.mathContext);
                     }
                 }
                 if (!temp.equals(BigDecimal.ZERO)){
@@ -371,7 +381,7 @@ class CSRMatrix {
                         //  if the col of the rowCurCol doesn't match the outer loop,
                         //  it means the parm[parmData[rowCurCol[data[k].col]].col][parmCol] == 0
                         //Thus, we don't need to include it in our calculation
-                        temp = temp.add(data[k].data.multiply(parmData[rowCurCol[data[k].col]].data));
+                        temp = temp.add(data[k].data.multiply(parmData[rowCurCol[data[k].col]].data, Main.mathContext), Main.mathContext);
                     }
                 }
 
@@ -416,7 +426,7 @@ class CSRMatrix {
             for(int curCol = 0; curCol < matrix.GetColSize(); curCol++){
                 BigDecimal temp = BigDecimal.ZERO;
                 for(int indx = row[curRow]; indx <= (curRow == row.length - 1 ? data.length - 1 : (row[curRow + 1] - 1)); indx++){
-                    temp = temp.add(data[indx].data.multiply(parmData[data[indx].col].GetEntry(curCol)));
+                    temp = temp.add(data[indx].data.multiply(parmData[data[indx].col].GetEntry(curCol)), Main.mathContext);
                 }
                 if (!temp.equals(BigDecimal.ZERO)){
                     nnz++;
@@ -432,7 +442,7 @@ class CSRMatrix {
             for(int curCol = 0; curCol < matrix.GetColSize(); curCol++){
                 BigDecimal temp = BigDecimal.ZERO;
                 for(int indx = row[curRow]; indx <= (curRow == row.length - 1 ? data.length - 1 : (row[curRow + 1] - 1)); indx++){
-                    temp = temp.add(data[indx].data.multiply(parmData[data[indx].col].GetEntry(curCol)));
+                    temp = temp.add(data[indx].data.multiply(parmData[data[indx].col].GetEntry(curCol)), Main.mathContext);
                 }
                 if (!temp.equals(BigDecimal.ZERO)){
                     result.data[curDataIndx++] = new Data(temp, curCol);
@@ -444,7 +454,7 @@ class CSRMatrix {
     }
 
     Vector IterationMethod(Vector rightSide){
-        final int STEP_LIMIT = 20;
+        final int STEP_LIMIT = 100;
         final double TOLERANCE = 10e-6;
         final int MAX_SEARCH_DIR = 50;
 
@@ -460,7 +470,7 @@ class CSRMatrix {
         Matrix pMatrix = new Matrix(curSearchDir, 2);
         Matrix bMatrix = new Matrix(this.TimeVector(curSearchDir), 2);
 
-        for(int step = 1; step < STEP_LIMIT && norm.compareTo(initialNorm.multiply(BigDecimal.valueOf(TOLERANCE))) > 0; step++){
+        for(int step = 1; step < STEP_LIMIT && norm.compareTo(initialNorm.multiply(BigDecimal.valueOf(TOLERANCE), Main.mathContext)) > 0; step++){
             //|r - bMatrix * alpha| -> min
             //<=> least square(r = bMatrix * alpha)
             //<=> least square(r = QR * alpha)
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -10,13 +11,17 @@ import java.util.Scanner;
 // ~~ Means for general purpose
 // ~! For debugging purpose
 public class Main {
-    static int BIGDECIMAL_SCALE = 10;
+    //NOTE: LOW BIGDECIMAL_SCALE CAN LEADS TO 0 AS ROUNDING RESULT!!!
+    static int BIGDECIMAL_SCALE = 20;                                           //Max decimal used
+    static MathContext mathContext = new MathContext(BIGDECIMAL_SCALE);         //Max decimal used in BigDecimal
 
     static final String WHITESPACE = "\\s+";
+    static int PRECISION = 7;                                                   //Max decimal to be displayed
+    static int MAX_SLOT = Math.max(10, PRECISION);                               //Max slot to display: 5.5 counts as 3 slots
     //static String testInput = "1 2 0 3\n0 5 6 4\n7 0 9 6\n0 0 0\n8 7 6 0";
     static String testInput2 = "1 1 0\n1 0 1\n0 1 1";
     static String testInput3 = "1 0 2 3\n0 3 5 0\n2 5 4 0";
-    static String iterationInput = "2 4 2 5\n5 4 1 10\n9 7 5 1\n22 48 15";
+    static String iterationInput = "2 4 2 5\n5 4 1 10\n9 7 5 1\n22 48 15 1";
     static String bIterationInput = "9 50 3 2";
 
     public static <T> T GetValueOrDefault(T value, T defaultValue) {
@@ -74,33 +79,37 @@ public class Main {
         CSRMatrix iterationCSR = new CSRMatrix(iterationInput);
         Print(iterationCSR.IterationMethod(new Vector(bIterationInput)), "Iteration Method with iteration CSR!");
 
-        Print(new CSRMatrix(ReadCSRFromFile("e05r0000.mtx")), "CSR From mtx file");
+        CSRMatrix fileCSR = new CSRMatrix(ReadCSRFromFile("e05r0000.mtx"));
+        Vector rhsVector = new Vector(ReadVectorFromFile("e05r0000_rhs1.mtx"));
+        Print(fileCSR, "CSR From mtx file");
+        Print(fileCSR.IterationMethod(rhsVector), "File matrix with right hand side");
+        //PrintData(fileCSR, "File CSR");
     }
 
-    static void IsCSRSymmetric(CSRMatrix parm, String name){
+    private static void IsCSRSymmetric(CSRMatrix parm, String name){
         if (CSRMatrix.IsSymmetric(parm)){
             System.out.println("~~" + name + " is symmetric!");
         } else {
             System.out.println("~~" + name + " is NOT symmetric!");
         }
     }
-    static void Print(CSRMatrix parm, String name){
+    private static void Print(CSRMatrix parm, String name){
         System.out.println("~~" + name);
         parm.Print();
     }
-    static void PrintData(CSRMatrix parm, String name){
+    private static void PrintData(CSRMatrix parm, String name){
         System.out.println("~!" + name + " data:");
         parm.PrintData();
     }
-    static void Print(Vector parm, String name){
+    private static void Print(Vector parm, String name){
         System.out.println("~~" + name);
         parm.Print();
     }
-    static void Print(Matrix parm, String name){
+    private static void Print(Matrix parm, String name){
         System.out.println("~~" + name);
         parm.Print();
     }
-    static void Print(LinkedList<Matrix> matrices, String name){
+    private static void Print(LinkedList<Matrix> matrices, String name){
         if (matrices.size() <= 0){
             System.out.println("~~Empty list!!!");
         }
@@ -126,7 +135,7 @@ public class Main {
             return currArr;
         }
         //Find the first element that is less than its previous element from right to left
-        int result[] = new int[currArr.length];
+        int[] result = new int[currArr.length];
         System.arraycopy(currArr, 0, result, 0, currArr.length);
         int indx = currArr.length - 2, indx2 = currArr.length - 1;
         for(; indx > 0; indx--){
@@ -182,7 +191,7 @@ public class Main {
         Scanner sc = new Scanner(file);
 
         //Initialization
-        String tempLine = "";
+        String tempLine;
         while((tempLine = sc.nextLine()) != null && tempLine.startsWith("%"));
         String[] temps = tempLine.split(" ");
         int row = Integer.parseInt(temps[0]);
@@ -198,23 +207,54 @@ public class Main {
         //Taking input in
         for(int indx = 0; indx < totalLine; indx++){
             int curRow = sc.nextInt() - 1;
-            int curCol = sc.nextInt();
-            double curData = sc.nextDouble();
+            int curCol = sc.nextInt() - 1;
+            String curData = sc.nextLine();
 
             if (lastCol[curRow] != curCol - 1){
                 //Some 0s are missing
-                for(int indx2 = 0; indx2 < curCol - lastCol[curRow]; indx2++){
+                for(int indx2 = lastCol[curRow] + 1; indx2 < curCol; indx2++){
                     rows[curRow].append(" " + 0);
                 }
             }
-            rows[curRow].append(" ");
             rows[curRow].append(curData);
             lastCol[curRow] = curCol;
+        }
+
+        //Fill out 0s
+        for(int curRow = 0; curRow < row; curRow++){
+            for(int indx = lastCol[curRow] + 1; indx < col; indx++){
+                rows[curRow].append(" 0");
+            }
         }
 
         for(int indx = 0; indx < row; indx++){
             result.append(rows[indx]);
             result.append("\n");
+        }
+
+        return result.toString();
+    }
+    private static String ReadVectorFromFile(String fileName) throws FileNotFoundException {
+        //Index start at 1!!!
+        //https://math.nist.gov/MatrixMarket/formats.html#mtx
+        StringBuilder result = new StringBuilder();
+
+        File file =
+                new File(fileName);
+        Scanner sc = new Scanner(file);
+
+        //Initialization
+        String tempLine;
+        while((tempLine = sc.nextLine()) != null && tempLine.startsWith("%"));
+        int row = Integer.parseInt(tempLine.split(" ")[0]);
+
+        //Taking input in
+        for(int indx = 0; indx < row; indx++){
+            String curData = sc.nextLine().replaceAll(WHITESPACE, "");
+
+            if (curData.equals(WHITESPACE)) continue;
+            result.append(curData);
+            result.append(" ");
         }
 
         return result.toString();
